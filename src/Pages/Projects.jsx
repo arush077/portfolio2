d811c395faec992b819431012c13a757916c5af4
arush077 <arushshetty07@gmail.com>
2024-02-06 01:01:42 +0530

    gg

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -70,7 +70,7 @@ import Project_Cards from "./Project_Cards";
 
     const handleDivClick_personal = () => {
       // Replace 'https://example.com' with the URL you want to redirect to
-      window.location.href = 'portfolio2-rosy-one.vercel.app';
+      window.location.href = '/';
     };
 
 
