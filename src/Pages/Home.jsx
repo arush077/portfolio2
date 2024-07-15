a7c872b8f8d153a7fc2fc80638e032ad21f89357
arush077 <arushshetty07@gmail.com>
2024-07-15 13:26:14 +0530

    Revert "Commented out extra page in home"
    This reverts commit f6b3a275a225e4c19250e2dfde331ee58424ef79.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -88,10 +88,9 @@ const Home = () => {
        
     </div>
 
-    {/* <div className='page2' id='hi_page2'>
+    <div className='page2' id='hi_page2'>
       <h1 className='animated-textx'>HI</h1>
-    </div> */}
-    
+    </div>
 
 
     </div>
