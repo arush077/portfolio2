e0eb570dcd7b80f0b4389624c38df0383d348db2
Arush Shetty <106901537+arush077@users.noreply.github.com>
2026-07-11 21:30:40 +0530

    fix: src/Pages/About.jsx — automated fix for #1

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -57,7 +57,7 @@ const About = () => {
 
 <ul className='hero3-ul'>
             <li className='hero3-hey'>About me: </li>
-            <li className='hero3-pre'>I'm a prefinal year student at BITS Pilani,Pilani Campus</li>
+            <li className='hero3-pre'>I'm a graduate from BITS Pilani, Pilani Campus, with SWE experience at Uber.</li>
             <li className='hero3-branch'>Branch: Electrical And Electronics</li>
         </ul>
     </div>
